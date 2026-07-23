29e9b207eecc6a0c50e6b345ebaf6f3ba3cd6b13
MushishiCoder <ljx960326@outlook.com>
2026-07-23 14:20:08 +0800

    用了逻辑表达式

---
+++ C_Language_Journey/branch_structures/leap_year/leap_year.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int year=0;
+    printf("输入一个年份:");
+    scanf("%d",&year);
+    if((year%4==0&&year%100==0)||year%400==0){
+        printf("Yes\n");
+    }else{
+        printf("No\n");
+    }
+    return 0; 
+}
