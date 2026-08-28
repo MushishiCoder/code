845768db4e7bd12d929771c335be4feeab3e905b
MushishiCoder <ljx960326@outlook.com>
2026-08-28 15:27:15 +0800

    用二维字符数组实现多个字符串的排序

--- C_Language_Journey/char_arrays_and_strings/string_array/string_sort/string_sort.c
+++ C_Language_Journey/char_arrays_and_strings/string_array/string_sort/string_sort.c
@@ -2,10 +2,10 @@
 #include<string.h>
 #include<stdlib.h>
 
-#define N 100
-#define LEN 81
+#define N 100                                   //定义最多可以存储多少字符串
+#define LEN 81                                  //定义每个字符串的最大长度（包括\0）    
 
-void Sort(char str[N][LEN],int n);
+void Sort(char str[N][LEN],int n);              //函数声明：对该字符串数组的n个字符串进行从小到大排序
 
 int main(void){
     int i,n;
@@ -17,7 +17,7 @@ int main(void){
 
     printf("请输入%d个字符串:\n",n);
     for(i=0;i<n;i++){
-        fgets(str[i],LEN,stdin);
+        fgets(str[i],LEN,stdin);                //输入该字符串数组，为保证后续字符串数组能够正常像矩阵一样输出，不需要去掉\n
     }
 
     Sort(str,n);
@@ -30,9 +30,9 @@ int main(void){
 
 void Sort(char str[N][LEN],int n){
     int i,j;
-    char temp[LEN];
+    char temp[LEN];                             //定义一个一维字符串，用于进行两个字符串得交换
 
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++){                           //利用双层嵌套for循环来进行比较
         for(j=i+1;j<n;j++){
             if(strcmp(str[i],str[j])>0){
                 strcpy(temp,str[i]);
